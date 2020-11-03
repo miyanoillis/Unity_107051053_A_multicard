b022c950b17cb1f9a6c4ff8040b00ee9c9167860
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-11-03 12:02:40 +0800

    write script "joystick control"

--- Unity_multicard/Assets/Scriot/GameManager.cs
+++ Unity_multicard/Assets/Scriot/GameManager.cs
@@ -2,6 +2,33 @@
 
 public class GameManager : MonoBehaviour
 {
+    [Header("物件")]
     public Transform ghost;
     public Transform covid19;
+    [Header("控制桿")]
+    public FloatingJoystick floatingJoystick;
+    [Header("速度"), Range(0.1f, 10f)]
+    public float speed = 10f;
+    [Header("大小"), Range(0.1f, 1f)]
+    public float size = 0.01f;
+
+    private void Start()
+    {
+        print("start");
+    }
+
+    private void Update()
+    {
+        if(Mathf.Abs(floatingJoystick.Vertical) < 0.5f)
+        { 
+        covid19.Rotate(0, -floatingJoystick.Horizontal * speed, 0);
+        }
+        if (Mathf.Abs(floatingJoystick.Horizontal) < 0.5f)
+        {
+            covid19.localScale += new Vector3(1, 1, 1)* floatingJoystick.Vertical* size;
+        }
+        
+
+        print(floatingJoystick.Vertical + "," + floatingJoystick.Horizontal);
+    }
 }
