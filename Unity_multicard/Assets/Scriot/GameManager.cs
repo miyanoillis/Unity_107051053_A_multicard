d17b64c77e96f4812b25eff1c201aaf78955e966
miyanoillis <71679915+miyanoillis@users.noreply.github.com>
2020-11-10 12:01:33 +0800

    complete and apk

--- Unity_multicard/Assets/Scriot/GameManager.cs
+++ Unity_multicard/Assets/Scriot/GameManager.cs
@@ -11,6 +11,9 @@ public class GameManager : MonoBehaviour
     public float speed = 10f;
     [Header("大小"), Range(0.1f, 1f)]
     public float size = 0.01f;
+    [Header("動畫元件")]
+    public Animator animghost;
+    public Animator animcovid19;
 
     private void Start()
     {
@@ -19,16 +22,30 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
+        
+
         if(Mathf.Abs(floatingJoystick.Vertical) < 0.5f)
         { 
         covid19.Rotate(0, -floatingJoystick.Horizontal * speed, 0);
+            ghost.Rotate(0, -floatingJoystick.Horizontal * speed, 0);
+
         }
         if (Mathf.Abs(floatingJoystick.Horizontal) < 0.5f)
         {
             covid19.localScale += new Vector3(1, 1, 1)* floatingJoystick.Vertical* size;
+            ghost.localScale += new Vector3(1, 1, 1) * floatingJoystick.Vertical * size;
+
         }
-        
+
+        covid19.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(covid19.localScale.x, 0.5f, 3.5f);
+        ghost.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(covid19.localScale.x, 0.5f, 3.5f);
 
         print(floatingJoystick.Vertical + "," + floatingJoystick.Horizontal);
+    }
+
+    public void Playanim(string Name)
+    {
+        animcovid19.SetTrigger(Name);
+        animghost.SetTrigger(Name);
     }
 }
